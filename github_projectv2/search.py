928972c2315d5e554679ff6dabbf8829d0054d4e
Chris Cornutt <enygma@github.com>
2023-03-03 12:44:57 -0700

    Adding a "search" class to use the search filtering ability on the API

---
+++ github_projectv2/search.py
@@ -0,0 +1,39 @@
+from github_projectv2.base import Base
+from github_projectv2.item import Item
+
+
+class Search(Base):
+    def issues(self, filter):
+        # Get the partial for the issue query
+        itemQuery = self.get_query("partial/item")
+
+        query = """{
+            search(
+                query: "%s"
+                type: ISSUE
+                first: 10
+            ) {
+                edges {
+                node {
+                    ... on Issue {
+                    %s
+                    }
+                }
+                }
+            }
+        }
+        """ % (
+            filter,
+            itemQuery,
+        )
+        results = self.run_query(query)
+
+        # Makes items out of the results
+        items = results["data"]["search"]["edges"]
+        returnItems = []
+        for index in range(len(items)):
+            node = items[index]["node"]
+            item = Item(node)
+            returnItems.append(item)
+
+        return returnItems
