f6752dc8d4b8bf9caa6e2a04ba3562b4fb926fc1
Chris Cornutt <enygma@github.com>
2024-06-24 11:46:48 -0600

    Updating search class to support pagination

--- github_projectv2/search.py
+++ github_projectv2/search.py
@@ -3,19 +3,32 @@ from github_projectv2.item import Item
 
 
 class Search(Base):
+    def __init__(self, node=None):
+        super().__init__()
+
+        self.first = 10
+        self.itemEndCursor = ""
+        self.hasNextPage = False
+
     def issues(self, filter):
         # Get the partial for the issue query
         template = self.jinja.get_template("partial/item.graphql")
         item_query = template.render(
             {"options": {"includeComments": True, "includeTimelineEvents": True}}
         )
+        start = 10
 
         query = """{
             search(
                 query: "%s"
                 type: ISSUE
-                first: 10
+                first: %s
+                after: AFTER
             ) {
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
                 edges {
                 node {
                     ... on Issue {
@@ -27,9 +40,24 @@ class Search(Base):
         }
         """ % (
             filter,
+            start,
             item_query,
         )
+
+        # Replace for our "AFTER" so we can paginate
+        query = query.replace(
+            "AFTER", '"{}"'.format(self.itemEndCursor) if self.itemEndCursor else "null"
+        )
         results = self.run_query(query)
+
+        # Get the page info to see about pagination
+        pageInfo = results["data"]["search"]["pageInfo"]
+        if pageInfo["hasNextPage"]:
+            self.hasNextPage = True
+            self.itemEndCursor = pageInfo["endCursor"]
+        else:
+            self.hasNextPage = False
+            self.itemEndCursor = ""
 
         # Makes items out of the results
         items = results["data"]["search"]["edges"]
