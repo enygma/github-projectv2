ee6950af4efd3ba7f3c43cfe4a694db83f2a9c2f
Chris Cornutt <enygma@github.com>
2025-01-29 10:43:35 -0700

    Updating the field class to inject the updated date and loading the value for select and text types

--- github_projectv2/field.py
+++ github_projectv2/field.py
@@ -8,15 +8,29 @@ class Field(Base):
     name = ""
     dataType = ""
     options = []
+    value = None
+    updated = None
 
     def __init__(self, node):
         super().__init__()
+        self.load(node)
 
+    def load(self, node):
         self.id = node.get("id")
         self.name = node.get("name")
         self.dataType = node.get("dataType")
         self.options = []
+        self.updated = node.get("updatedAt")
 
         if self.dataType == "SINGLE_SELECT":
+            # If we have options, load them
             for option in node.get("options"):
                 self.options.append(Option(option))
+
+    def load_value(self, node):
+        if self.dataType == "SINGLE_SELECT":
+            self.value = Option({"id": node.get("optionId"), "name": node.get("name")})
+
+        if self.dataType == "TEXT":
+            if node.get("text") is not None:
+                self.value = node.get("text")
